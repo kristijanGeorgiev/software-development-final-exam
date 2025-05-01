813dc883c2ea820aca18eef1fd29b081fb945712
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-05-01 19:03:35 +0200

    Update database_setup.dart

--- data_sembast/lib/setup/database_setup.dart
+++ data_sembast/lib/setup/database_setup.dart
@@ -1,4 +1,3 @@
-import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
@@ -9,10 +8,7 @@ class AppDatabase {
 
   static Future<Database> get database async {
     if (_db == null) {
-      final dir = await getApplicationDocumentsDirectory();
       await dir.create(recursive: true);
-      final dbPath = join(dir.path, 'students.db');
-      _db = await databaseFactoryIo.openDatabase(dbPath);
     }
     return _db!;
   }
